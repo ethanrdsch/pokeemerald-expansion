41bd4d8fa4fe4bdef894eda6e063ca7211c028c9
ethanrdsch <ethanrdsch01@gmail.com>
2024-07-23 11:57:43 -0400

    removed rage from universal moves

--- src/pokemon.c
+++ src/pokemon.c
@@ -5156,7 +5156,6 @@ static const u16 sUniversalMoves[] =
     MOVE_HIDDEN_POWER,
     MOVE_MIMIC,
     MOVE_NATURAL_GIFT,
-    MOVE_RAGE,
     MOVE_RETURN,
     MOVE_SECRET_POWER,
     MOVE_SUBSTITUTE,
